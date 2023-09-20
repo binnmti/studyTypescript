b2e277ab63825b3bba613ee1a13dae5839e80856
bin matsui <BinMatsui@hotmail.com>
2023-09-21 00:03:05 +0900

    function movePointer

--- src/usagecases/question5.ts
+++ src/usagecases/question5.ts
@@ -1,38 +1,33 @@
 export const question5 = (str1: string, str2: string): boolean => {
-    let point1 = str1.length - 1;
-    let point2 = str2.length - 1;
-    let sharp1Counter = 0;
-    let sharp2Counter = 0;
-    while (point1 >= 0 || point2 >= 0) {
-        if (str1[point1] === '#') {
-            sharp1Counter++;
-            point1--;
-            continue;
-        }
-        if (sharp1Counter > 0) {
-            sharp1Counter--;
-            point1--;
-            continue;
-        }
-        if (str2[point2] === '#') {
-            sharp2Counter++;
-            point2--;
-            continue;
-        }
-        if (sharp2Counter > 0) {
-            sharp2Counter--;
-            point2--;
-            continue;
-        }
-        if (str1[point1] !== str2[point2]) {
+    let pointer1 = str1.length - 1;
+    let pointer2 = str2.length - 1;
+    while (pointer1 >= 0 || pointer2 >= 0) {
+        pointer1 = movePointer(str1, pointer1);
+        pointer2 = movePointer(str2, pointer2);
+        if (str1[pointer1] !== str2[pointer2]) {
             return false;
         }
-        if (point1 >= 0) {
-            point1--;
-        }
-        if (point2 >= 0) {
-            point2--;
-        }
+        pointer1--;
+        pointer2--;
     }
     return true;
+};
+
+const movePointer = (target: string, startAt: number): number => {
+    let pointer = startAt;
+    let sharpCounter = 0;
+    while (pointer >= 0) {
+        if (target[pointer] === '#') {
+            sharpCounter++;
+            pointer--;
+            continue;
+        }
+        if (sharpCounter > 0) {
+            sharpCounter--;
+            pointer--;
+            continue;
+        }
+        break;
+    }
+    return pointer;
 };
