1a166b557b1eae86affb657917484eb1b84e3886
bin matsui <BinMatsui@hotmail.com>
2023-10-09 21:56:30 +0900

    q6 clear

--- src/usagecases/__tests__/question6.spec.ts
+++ src/usagecases/__tests__/question6.spec.ts
@@ -1,6 +1,12 @@
 import { question6 } from '../question6';
 describe('question6', () => {
-    it('color', () => {
+    it('2', () => {
         expect(question6('aaabbccc')).toBe(2);
     });
+    it('31', () => {
+        expect(question6('dasadjwlqdoqwfewfharphfgrewrhowhhfhsdfohfoihoasfdsadhkdfkjlbavjblvxnbknogwrelgjfheowafphpwohoidfhodhfo')).toBe(31);
+    });
+    // it('alotof', () => {
+    //     expect(question6('abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz')).toBe(259);
+    // });
 });
--- src/usagecases/question6.ts
+++ src/usagecases/question6.ts
@@ -1,14 +1,54 @@
-export const question6 = (str: string): number => {
-    //str "aaabbccc";
-    const map = new Map<string, number>;
-    for (const s of str) {
-        let co = map.get(s);
-        map.set(s, co === undefined ? 1 : ++co);
-    }
-    //a:3, b:2, c:3
-    for (const [key, value] of map) {
-        
-    }
+/*
+アルファベット小文字で構成される文字列sが与えられます。 文字列sの中に出現する各アルファベットの出現頻度が重複しないようにしたいです。
+重複する出現頻度の文字が存在する場合、何文字か削除します。 何文字削除すれば、全ての文字の出現頻度がユニークになるか調べてください。
+削除した結果として、その文字がなくなっても良いです。
 
-    return 2;
+アルファベット小文字のみ
+1 <= s.length <= 1000
+TypeScriptでは ts-heapq の使用が認められています。
+例1:
+s = "aaabbccc"
+  ↓
+"a" を1文字削除
+  ↓
+"aabbccc"
+  ↓
+"a"を1文字削除
+  ↓
+"abbccc"
+
+answer = 2文字
+*/
+export const question6 = (str: string): number => {
+    const charCounterMap = new Map<string, number>;
+    for (const s of str) {
+        let counter = charCounterMap.get(s);
+        charCounterMap.set(s, counter === undefined ? 1 : ++counter);
+    }
+    let deleteCounter = 0;
+    //a:3, b:2, c:3
+    for (let [s1, counter1] of charCounterMap) {
+        let sameCounter = false;
+        
+        while (true) {
+            for (const [s2, counter2] of charCounterMap) {
+                if (s1 === s2) {
+                    continue;
+                }
+                if (counter1 === counter2) {
+                    sameCounter = true;
+                    counter1--;
+                    charCounterMap.set(s1, counter1);
+                    deleteCounter++;
+                    break;
+                }
+            }
+            if (counter1 === 0 || sameCounter === false) {
+                break;
+            } else {
+                sameCounter = false;
+            }
+        }
+    }
+    return deleteCounter;
 };
