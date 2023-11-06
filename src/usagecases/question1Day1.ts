30a1d3ed1f89ccd8e912c85b1468cf4276b11d1a
bin matsui <BinMatsui@hotmail.com>
2023-11-06 22:23:43 +0900

    sort

--- src/usagecases/question1Day1.ts
+++ src/usagecases/question1Day1.ts
@@ -12,11 +12,10 @@ schedules.length < 1000
 schedules[i].length = 2
 schedules[i][0] < schedules[i][1]
 */
-//時間計算量 O(n^2)
-//空間計算量 O(1)
-export const sort = (schedules: number[][]): number[][] => {
-    for (let i = 0; i < schedules.length; i++) {
-        for (let j = schedules.length - 1; j > i; j--) {
+//バブルソート:時間計算量 O(n^2), 空間計算量 O(1)
+export const bubbleSort = (schedules: number[][], start: number, end: number): number[][] => {
+    for (let i = start; i <= end; i++) {
+        for (let j = end; j > start; j--) {
             if (schedules[j][0] < schedules[j - 1][0]) {
                 [schedules[j],schedules[j - 1]] = [schedules[j - 1],schedules[j]];
             }
@@ -25,13 +24,64 @@ export const sort = (schedules: number[][]): number[][] => {
     return schedules;
 }
 
-export const question1Day1 = (schedules: number[][]): boolean => {
-    schedules = sort(schedules);
-    for (let i = 0; i < schedules.length; i++) {
-        for (let j = i + 1; j < schedules.length; j++) {
-            if (schedules[i][1] > schedules[j][0]) {
-                return false;
+//選択ソート:時間計算量 O(n^2), 空間計算量 O(1)
+export const selectionSort = (schedules: number[][], start: number, end: number): number[][] => {
+    for (let i = start; i <= end; i++) {
+        let min = i;
+        for (let j = i + 1; j <= end; j++) {
+            if (schedules[min][0] > schedules[j][0]) {
+                min = j;
             }
+        }
+        [schedules[i],schedules[min]] = [schedules[min],schedules[i]];
+    }
+    return schedules;
+}
+
+//挿入ソート:時間計算量 O(n^2), 空間計算量 O(1)
+export const insertionSort = (schedules: number[][], start: number, end: number): number[][] => {
+    for (let i = start + 1; i <= end; i++) {
+        let hit = i;
+        for (let j = start; j < i; j++) {
+            if (schedules[hit][0] < schedules[j][0]) {
+                hit = j;
+                break;
+            }
+        }
+        //これはswap,したいのはinsert
+        // for (let j = hit; j < i; j++) {
+        //     [schedules[j],schedules[j + 1]] = [schedules[j + 1],schedules[j]];
+        // }
+    }
+    return schedules;
+}
+
+//マージソート:時間計算量 O(n long n), 空間計算量 O(1)
+export const mergeSort = (schedules: number[][], start: number, end: number): number[][] => {
+    const middle = Math.floor((end - start) / 2);
+    if (middle >= 2) {
+        mergeSort(schedules, start, start + middle);
+        mergeSort(schedules, end - middle, end);
+    }
+    bubbleSort(schedules, start, middle);
+    bubbleSort(schedules, middle, end);
+    return schedules;
+}
+
+//クイックソート:時間計算量 O(n^2), 空間計算量 O(1)
+export const quickSort = (schedules: number[][], start: number, end: number): number[][] => {
+}
+
+export const question1Day1 = (schedules: number[][]): boolean => {
+    // o schedules = bubbleSort(schedules, 0, schedules.length - 1);
+    // o schedules = selectionSort(schedules, 0, schedules.length - 1);
+    // o schedules = mergeSort(schedules, 0, schedules.length - 1);
+    // x schedules = insertionSort(schedules, 0, schedules.length - 1);
+
+    schedules = quickSort(schedules, 0, schedules.length - 1);
+    for (let i = 0; i < schedules.length - 1; i++) {
+        if (schedules[i][1] > schedules[i + 1][0]) {
+            return false;
         }
     }
     return true;
