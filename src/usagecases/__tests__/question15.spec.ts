b1bbc4a9455be89cf956dc9e323b1f9f9031b9a0
bin matsui <BinMatsui@hotmail.com>
2024-02-25 11:24:53 +0900

    q15

---
+++ src/usagecases/__tests__/question15.spec.ts
@@ -0,0 +1,12 @@
+import { question15 } from '../question15';
+describe('question15', () => {
+    it('test1', () => {
+        expect(question15([
+            [3, 2, 1],
+            [6, 4, 2]
+        ])).toBe(8);
+    });
+});
+
+
+
